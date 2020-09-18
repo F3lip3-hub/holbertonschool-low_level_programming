8be1c7552eae725236679c8fd66f591567bab05a
F3lip3-hub <88.figueroa@gmail.com>
2020-09-18 23:37:13 +0000

    Puts messenger

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
 int main()
-{
-  puts ("\"Programing is like building a multilingual puzzle");
-  return(0);
-}
+	puts ("\"Programing is like building a multilingual puzzle");
+	return(0);
