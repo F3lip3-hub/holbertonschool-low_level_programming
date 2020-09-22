ea938eb10efb51b32c9ee91f8de902254585ced1
F3lip3 <88.figueroa@gmail.com>
2020-09-22 15:26:28 +0000

    Print with putchar

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,18 +1,15 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+#include <header.h>
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
-int main(void) {
-	char H;
-	
-
-	for(H = 'Holberton'; H == 'H';;) {
-		putchar(H);
-	}
-	
-	return 0;
- 
+int main(void)
+{
+	char = H;
+	H = 'Holberton';
+	putchar('H');
+	return (0);
 }
 
