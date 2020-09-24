6e5594534e47f3afa811479deac7690f1f9c47e5
F3lip3 <88.figueroa@gmail.com>
2020-09-24 18:11:34 +0000

    Positive or negative guess

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+* main - Random number guess
+*
+* Return: 0
+*/
 int main(void)
 {
 	int n;
