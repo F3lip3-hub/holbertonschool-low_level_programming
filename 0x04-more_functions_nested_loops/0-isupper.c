82d315f33fd83b666a5f0fa92ac1cb73acc3213d
F3lip3 <88.figueroa@gmail.com>
2020-09-24 22:07:56 +0000

    What

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+* _isupper - Uppercase character
+*
+*
+* Return: If uppercase is true return 1 else 0
+*/
+int _isupper(int c)
+{
+
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (0);
+
+
+	}
+		else
+		{
+			return (0);
+		}
+}
