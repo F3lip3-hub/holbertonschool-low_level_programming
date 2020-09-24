99ecb867668d0b6fc68cb8dbd20c54586ee6059b
F3lip3 <88.figueroa@gmail.com>
2020-09-24 19:07:40 +0000

    Lowercase alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+* main - Alphabet
+*
+*
+* Return: 0
+*/
+int main(void)
+{
+	char L;
+
+	for (L = 'a'; L <= 'z'; ++L)
+	{
+		_putchar(L);
+	}
+	_putchar('\n');
+	return (0);
+}
+
