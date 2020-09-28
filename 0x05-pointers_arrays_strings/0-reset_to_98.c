6b81a6106497dce20a4a7d2a3512b52f2f9fcfe0
F3lip3 <88.figueroa@gmail.com>
2020-09-28 21:24:20 +0000

    Update value with pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+* reset_to_98 - Change value with pointer
+*
+* 
+*
+* Return: 'n' changes at the end
+*/
+
+void reset_to_98(int *n)
+{
+	
+	*n = 98;
+
+}
