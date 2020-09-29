1333e0de56f2a4a657ef36fa4d4a11f1bdc7c732
F3lip3 <88.figueroa@gmail.com>
2020-09-29 13:42:24 +0000

    Header file

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,5 +1,14 @@
 int _putchar(char c);
 
 
-/* 0x05 low level programming */
+/* 0 - 0x05 low level programming */
 void reset_to_98(int *n);
+
+/* 1 - 0x05*/
+void swap_int(int *a, int *b);
+
+/* 2 - 0x05 */
+int _strlen(char *s);
+
+/* 3 - 0x05 */
+void _puts(char *str);
