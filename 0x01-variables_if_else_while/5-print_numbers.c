9ad666004c6a733bbed67f9e5f084f2835dc9c24
F3lip3 <88.figueroa@gmail.com>
2020-09-30 20:17:01 +0000

    Print and exclude

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - prints all single digit numbers of base 10
+*
+*
+* Return: ALways 0
+*
+*/
+int main(void)
+{
+	int n;
+
+	n = 0;
+
+	while (n <= 9)
+	{
+		printf("%d", n);
+		n++;
+	}
+	printf("\n");
+	return (0);
+}
