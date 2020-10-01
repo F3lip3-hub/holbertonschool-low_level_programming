a317298eaca87d65ea4b43d5d265052b72b925ad
F3lip3 <88.figueroa@gmail.com>
2020-10-01 22:33:43 +0000

    Lets go

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+* _strcat - function to strcat
+*
+* @dest: one pointer
+* @src: second pointer
+*
+* Return: to the dest
+*/
+char  *_strcat(char *dest, char *src)
+{
+	int s = 0;
+	int t;
+
+	while (dest[s] != '\0')
+	{
+		s++;
+	}
+	for (t = 0; src[t] != '\0'; t++)
+	{
+		dest[s] = src[t];
+		s++;
+	}
+
+	dest[s] = '\0';
+
+	return (dest);
+}
