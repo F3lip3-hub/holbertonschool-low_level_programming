b59407759b7697899dd7242432fded1d472e1ba7
F3lip3 <88.figueroa@gmail.com>
2020-10-05 18:56:28 +0000

    Multiply the variables

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+* mul - multiplies the variables
+* @a: variable to mul
+* @b: variable to mul
+*
+* Return: always zero
+*/
+int mul(int a, int b)
+{
+	int r;
+	{
+	r = (a * b);
+	}
+	return (r);
+}
