2ee54a75d584004cd2eba1f297dca5f072165132
F3lip3 <88.figueroa@gmail.com>
2020-10-05 17:39:08 +0000

    Header file

---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,16 @@
+
+/* main function */
+int main(void);
+
+/* putchar function */
+int _putchar(char c);
+
+/* Print Alphabet */
+void print_alphabet(void);
+
+
+/* 0x04  more functions and loopies */
+int _isupper(int c);
+
+
+
