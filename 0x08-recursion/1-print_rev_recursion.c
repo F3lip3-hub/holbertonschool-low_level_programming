8c202515c02ffa36f5b773142a9d02a07fc35334
F3lip3 <88.figueroa@gmail.com>
2020-10-07 23:50:19 +0000

    Reversed recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+* _print_rev_recursion - Printing reverse
+* @s: Holding string
+*
+* 
+*/
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_puts_recursion((s + 1));
+	_putchar(*s);
+
+}
