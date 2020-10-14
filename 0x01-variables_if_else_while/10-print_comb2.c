87a71159c6c11ef1680f26a05c0aa479635f3b8e
F3lip3 <88.figueroa@gmail.com>
2020-10-14 15:23:48 +0000

    Double digit to 99

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -8,11 +8,11 @@
 int main(void)
 {
 	int i;
-  
+
 	for (i = 0; i <= 99; i++)
 	{
-		putchar((i/10)+'0');
-		putchar((i%10)+'0');
+		putchar((i / 10) + '0');
+		putchar((i % 10) + '0');
 		if (i <= 98)
 		{
 			putchar(',');
