3db1c2b17714be4c47d4790f53824e68c341bd38
F3lip3 <88.figueroa@gmail.com>
2020-10-14 15:20:09 +0000

    Double digit to 99

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+* main - putchar spaces and commas
+*
+*
+* Return: Always 0
+*/
+int main(void)
+{
+	int i;
+  
+	for (i = 0; i <= 99; i++)
+	{
+		putchar((i/10)+'0');
+		putchar((i%10)+'0');
+		if (i <= 98)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
