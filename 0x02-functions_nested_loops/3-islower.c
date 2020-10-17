6a8011b3355bc741e916091a738d681966d435b5
F3lip3 <88.figueroa@gmail.com>
2020-10-17 21:31:31 +0000

    Lowercase loop

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,19 +1,17 @@
 #include "holberton.h"
-/*
+/**
 * _islower - prototype for lowecase loop
 * @c: integer variable
-* Return 1 if true  0 if its false
-*
+* Return: 1 if true  0 if its false
 */
 int _islower(int c)
 {
-	if ( c >= 'a' && c <= 'z')
+	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
 	}
 	else
 	{
-
 		return (0);
 	}
 }
