f653fce0724d43f4f421e84ce0bd430f36034f2a
F3lip3 <88.figueroa@gmail.com>
2020-10-21 14:47:58 +0000

    project 0x0E

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+*
+* main -prototype function
+* @argc: argument count
+* @argv: argument vector
+* Result: 0 always
+*/
+int main(int argc, char *argv)
+{
+	
+	for (g = 0; g < argc; argc++)
+	{	
+		printf("%s\n", argv[g]);
+	}
+	return(0);
+}
