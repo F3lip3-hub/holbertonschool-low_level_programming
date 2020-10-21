6cc362861eb980ea030d5664a3f66ffef1ae27ef
F3lip3 <88.figueroa@gmail.com>
2020-10-21 22:18:06 +0000

    Absolute number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+* _abs - prototype function
+* @num: variable to compare
+* Return: the variable created
+*/
+int _abs(int num)
+{
+
+	if (num >= 0)
+		return (num);
+	return (num);
+}
