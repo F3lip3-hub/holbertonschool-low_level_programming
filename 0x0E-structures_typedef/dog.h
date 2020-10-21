5e081793d7fa6627fae5baad30f2f459d531a916
F3lip3 <88.figueroa@gmail.com>
2020-10-21 17:22:30 +0000

    Dog header struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+* struct dog - named dog
+* name: name of struct
+* float: age number
+* owner: owner name
+*/
 
-struct dog {
-	char *name;
-	float age;
-	char *owner;
+struct dog
+{
+char *name;
+float age;
+char *owner;
 };
 typedef struct dog dog;
 #endif
