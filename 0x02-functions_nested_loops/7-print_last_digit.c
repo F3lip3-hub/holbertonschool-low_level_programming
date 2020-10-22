003ade046daeaccef4b972dea940a0ce941918d4
F3lip3 <88.figueroa@gmail.com>
2020-10-22 23:23:11 +0000

    Last digit printed

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+* print_last_digit - prototype for last digit function
+* @l: variable type integer
+* Return: the last digit
+*/
+int print_last_digit(int l)
+{
+	int last = (l % 10);
+
+	return (last);
+}
