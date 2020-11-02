623d1babbc2378ef1b51dac06dd6935964bab231
F3lip3 <88.figueroa@gmail.com>
2020-11-02 16:55:27 +0000

    Task 1 update

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* main - prototype function to return argcount
+* @argc: argument count
+* @argv: argument vector
+* Return: main program name
+*/
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc);
+	return (0);
+}
+
