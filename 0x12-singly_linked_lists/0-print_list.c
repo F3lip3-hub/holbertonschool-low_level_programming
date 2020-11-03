31f9bcef5d1e9d2d03dd5c21630536e70d055a9c
F3lip3 <88.figueroa@gmail.com>
2020-11-03 15:50:15 +0000

    Update project

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,20 +12,18 @@ size_t print_list(const list_t *h)
 	{
 		return (0);
 	}
-
 	while (h != NULL)
 	{
 		if (h->str == NULL)
 		{
 			printf("[0] (nil)\n");
 		}
-
 		else
 			printf("[%i] %s\n", h->len, h->str);
 
-			h = h->next;
+		h = h->next;
 		len++;
 	}
-	printf("\n");
+
 	return (len);
 }
