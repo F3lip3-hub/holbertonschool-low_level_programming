77619e034bbb02696f5f32b932873e2a157b3cd4
F3lip3 <88.figueroa@gmail.com>
2020-11-03 19:37:00 +0000

    Update existing head node

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * add_nodeint - Prototype to add a new node
+ * @n: variable constant
+ * @head: start of the list
+ * Return: new node
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_head;
+
+	new_head = malloc(sizeof(listint_t));
+
+	if (new_head == NULL)
+	{
+		return (NULL);
+	}
+	new_head->n = n;
+	new_head->next = *head;
+	*head = new_head;
+	return (new_head);
+}
