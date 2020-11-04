d373a9fb7937759a1c12afd7cf9beb8587f794d9
F3lip3 <88.figueroa@gmail.com>
2020-11-04 15:05:49 +0000

    Update task 1 fixed

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,6 @@
 #include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
 /**
  * init_dog - prototype with struct dog as parameter
  * @d: pointer to the struct
@@ -9,9 +11,7 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
-	{
 		return;
-	}
 
 	d->name = name;
 	d->age = age;
