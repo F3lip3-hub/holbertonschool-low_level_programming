19716f175789fe9c10514158b7541c8bfb082227
F3lip3 <88.figueroa@gmail.com>
2020-11-04 03:13:01 +0000

    Update header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,6 +14,12 @@ typedef struct dog
 	char *owner;
 } my_dog;
 
+typedef struct new_dog
+{
+
+
+} dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
 void print_dog(struct dog *d);
